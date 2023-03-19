0d4e8f4c24f352833a414aef05576ece529d6d46
KJM <kjm99d@naver.com>
2023-03-19 13:54:03 +0900

    ✨ HTTP 객체생성 API 추가

---
+++ HTTPAPI.cpp
@@ -0,0 +1,16 @@
+﻿#include "HTTPAPI.h"
+
+void HTTP_API::Create_HTTP(Request** ppObj, wchar_t * url)
+{
+    (*ppObj) = new Request(url);
+
+    return void();
+}
+
+void HTTP_API::Delete_HTTP(Request** ppObj)
+{
+    delete (*ppObj);
+    (*ppObj) = nullptr;
+
+    return void();
+}
---
+++ HTTPAPI.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "Request.h"
+
+#if WINSYSHTTP_EXPORTS
+#define WINSYSHTTP_API _declspec(dllexport)
+#else
+#define WINSYSHTTP_API _declspec(dllexport)
+#endif
+
+namespace HTTP_API
+{
+	typedef void(*fp_Create_HTTP)(Request** ppObj, wchar_t* url);
+	typedef void(*fp_Delete_HTTP)(Request** ppObj);
+
+	/**
+	 * @brief 새 HTTP 요청 객체를 생성합니다.
+	 * @param ppObj Request 객체를 가리키는 포인터의 포인터입니다.
+	 * @param url 요청을 보낼 URL입니다.
+	 */
+	WINSYSHTTP_API void Create_HTTP(Request** ppObj, wchar_t* url);
+	/**
+	 * @brief HTTP 요청 객체를 삭제합니다.
+	 * @param ppObj Request 객체를 가리키는 포인터의 포인터입니다.
+	 */
+	WINSYSHTTP_API void Delete_HTTP(Request** ppObj);
+}
